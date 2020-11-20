5ebb7aafea3218e4361fe666a56ba337c537e3a4
Ilmari Kumpula <ilmari.kumpula@gmail.com>
2020-11-20 11:47:07 +0200

    CSS-bundling now working correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import * as React from "react";
+import "./index.css";
 
 export default () => (
     <>
       <h3>Esbuild React App</h3>
-      <h4>Minimal possible Esbuild-template project</h4>
+      <h4>Minimal Esbuild template app!</h4>
     </>
   );
